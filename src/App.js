a920c81c20b925461d256512bd04c3f6c24939c0
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2024-01-15 07:32:12 -0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import Nome from "./components/Nome";
+import { useState } from "react";
+
+// import Nome from "./components/Nome";
 
 function App() {
+  const [aluno, setAluno] = useState("Rocker Tech");
+
+  function handleChangeName(nome) {
+    setAluno(nome);
+  }
+
   return (
     <div>
       <h1>React app</h1>
-      <Nome aluno="Ricardo" idade={28}/>
+      <h2>Olá: {aluno}</h2>
+      <button onClick={() => handleChangeName("Ricardo Rocker")}>
+        Mudar nome
+      </button>
+      {/* <Nome aluno="Ricardo" idade={28} />
       <br />
-      <Nome aluno="Petrucia" idade={26}/>
+      <Nome aluno="Petrucia" idade={26} /> */}
     </div>
   );
 }
